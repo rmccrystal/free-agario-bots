0fd6556e3928e347c1ba44a2f62c456f0fe2ebe0
Legend Mod <jimboy3100@hotmail.com>
2019-09-01 18:13:20 +0300

    LATEST UPDATE

--- free-agario-bots.ogario.user.js
+++ free-agario-bots.ogario.user.js
@@ -10,7 +10,8 @@
 // @connect      cdn.ogario.ovh
 // ==/UserScript==
 
-const CLIENT_VERSION = '1.0.9'
+
+const CLIENT_VERSION = GM_info.script.version
 
 if(location.host === 'agar.io' && location.pathname === '/'){
     location.href = `https://agar.io/ogario${location.hash}`
@@ -20,7 +21,7 @@ if(location.host === 'agar.io' && location.pathname === '/'){
 function modifyHTML(html){
     return html
         .replace('<head>', '<head><script src="https://bundle.run/buffer@5.2.1"></script><script src="https://pastebin.com/raw/z9hBsFYi"></script>')
-        .replace('https://cdn.ogario.ovh/v4/beta/ogario.v4.js', 'https://raw.githubusercontent.com/xN3BULA/free-agario-bots/master/ogario.js')
+        .replace('https://cdn.ogario.ovh/v4/beta/ogario.v4.js', 'https://pastebin.com/raw/qguS2wyP')
 }
 
 if(!navigator.userAgent.includes('Chrome/') || Number(navigator.userAgent.match(/Chrome\/(\d+)/)[1]) < 76) window.stop()
